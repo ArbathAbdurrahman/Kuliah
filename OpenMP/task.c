7b175551fe9e5c71483e3b2632de8b4572dc12b8
Arbath Abdurrahman <arbath21@gmail.com>
2024-10-18 08:20:14 +0700

    Tugas Membandingkan parallel vs serial

--- OpenMP/task.c
+++ OpenMP/task.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <omp.h>
 
+// loop 1-100 5x
+
 int main() {
     omp_set_num_threads(5); // Set thread
     int loop = 100; // Jumlah Loop
@@ -48,92 +50,92 @@ int main() {
                 }
                 printf("Task 5 completed by thread %d\n", omp_get_thread_num());
             }
-            // Task 6
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 6 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 6 completed by thread %d\n", omp_get_thread_num());
-            }
+            // // Task 6
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 6 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 6 completed by thread %d\n", omp_get_thread_num());
+            // }
 
-            // Task 7
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 7 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 7 completed by thread %d\n", omp_get_thread_num());
-            }
+            // // Task 7
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 7 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 7 completed by thread %d\n", omp_get_thread_num());
+            // }
 
-            // Task 8
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 8 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 8 completed by thread %d\n", omp_get_thread_num());
-            }
+            // // Task 8
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 8 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 8 completed by thread %d\n", omp_get_thread_num());
+            // }
 
-            // Task 9
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 9 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 9 completed by thread %d\n", omp_get_thread_num());
-            }
+            // // Task 9
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 9 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 9 completed by thread %d\n", omp_get_thread_num());
+            // }
 
-            // Task 10
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 10 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 10 completed by thread %d\n", omp_get_thread_num());
-            }
+            // // Task 10
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 10 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 10 completed by thread %d\n", omp_get_thread_num());
+            // }
 
-            // Task 11
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 11 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 11 completed by thread %d\n", omp_get_thread_num());
-            }
+            // // Task 11
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 11 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 11 completed by thread %d\n", omp_get_thread_num());
+            // }
 
-            // Task 12
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 12 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 12 completed by thread %d\n", omp_get_thread_num());
-            }
-            // Task 13
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 13 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 13 completed by thread %d\n", omp_get_thread_num());
-            }
-            // Task 14
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 14 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 14 completed by thread %d\n", omp_get_thread_num());
-            }
-            // Task 15
-            #pragma omp task
-            {
-                for (int i = 1; i <= loop; i++) {
-                    printf("Task 15 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
-                }
-                printf("Task 15 completed by thread %d\n", omp_get_thread_num());
-            }
+            // // Task 12
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 12 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 12 completed by thread %d\n", omp_get_thread_num());
+            // }
+            // // Task 13
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 13 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 13 completed by thread %d\n", omp_get_thread_num());
+            // }
+            // // Task 14
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 14 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 14 completed by thread %d\n", omp_get_thread_num());
+            // }
+            // // Task 15
+            // #pragma omp task
+            // {
+            //     for (int i = 1; i <= loop; i++) {
+            //         printf("Task 15 - Loop iteration: %d by thread %d\n", i, omp_get_thread_num());
+            //     }
+            //     printf("Task 15 completed by thread %d\n", omp_get_thread_num());
+            // }
 
             #pragma omp taskwait // Tunggu hingga semua task selesai
             printf("All parallel tasks completed.\n");
